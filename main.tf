908d1cb0d12e385387d821c7843ac9631044321b
pypeaday <pypeaday@pype.dev>
2024-10-16 10:24:43 -0500

    ignore the data secret for now, call it good and simple

--- main.tf
+++ main.tf
@@ -92,14 +92,17 @@ resource "helm_release" "argocd" {
 }
 
 
-data "kubernetes_secret" "argocd_admin" {
+# TODO: cannot read this... something is not configured right with hitting the k8s api and I'm just not sure what it is
+# │ Error: Get "http://localhost/api/v1/namespaces/argocd/secrets/argocd-initial-admin-secret": dial tcp [::1]:80: connect: connection refused
 
-  metadata {
-    name      = "argocd-initial-admin-secret"
-    namespace = helm_release.argocd.namespace
-  }
+# data "kubernetes_secret" "argocd_admin" {
 
-}
+#   metadata {
+#     name      = "argocd-initial-admin-secret"
+#     namespace = helm_release.argocd.namespace
+#   }
+
+# }
 
 # provider "argocd" {
 
