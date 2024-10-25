b092a7ea68e5cb7ddb0df4f37949bb3fb61570dd
pypeaday <pypeaday@pype.dev>
2024-10-25 13:59:56 -0500

    no remote backend storage from kestra until minio is figured out

--- providers.tf
+++ providers.tf
@@ -13,14 +13,14 @@ terraform {
       version = "2.16.0"
     }
   }
-  backend "s3" {
-    endpoints = {
-      s3 = "http://192.168.1.9:9004" # Minio endpoint
-    }
-    # NOTE: check readme for terraform init information
-    bucket = "terraform-homelab-k8s"
-    key    = "terraform.state"
-    region = "main"
+  # backend "s3" {
+  #   endpoints = {
+  #     s3 = "http://192.168.1.9:9004" # Minio endpoint
+  #   }
+  #   # NOTE: check readme for terraform init information
+  #   bucket = "terraform-homelab-k8s"
+  #   key    = "terraform.state"
+  #   region = "main"
 
   #   skip_credentials_validation = true
   #   skip_metadata_api_check     = true
